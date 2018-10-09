3d92009ad5c353467a08922a8b691c20fe8d2394
Sharad Vellore <43963226+ript1d5@users.noreply.github.com>
2018-10-09 17:03:01 -0400

    New Class Files

---
+++ AutoWand/AutoWand/Customer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace AutoWand
+{   //customer class holds all personal details of a customer so that they can be via database
+    [XmlRoot(ElementName = "Customer")]
+    public class Customer
+    {
+        //name
+        [XmlAttribute(DataType = "string")]
+        public string firstName { get; set; }
+        [XmlAttribute(DataType = "string")]
+        public string lastName { get; set; }
+
+        //address class
+        [XmlAttribute(DataType = "string")]
+        public string custAddr { get; set; } //we will simply take all of the different address elements
+                                               //and add them to a single string
+        //personal contact details
+        [XmlAttribute(DataType = "int")]
+        public int phoneNumber { get; set; }
+        [XmlAttribute(DataType = "string")]
+        public string emailAddress { get; set; }
+
+        public Customer()
+        {
+
+        }
+
+        public Customer(string firstName, string lastName, string custAddr, int phoneNumber, string emailAddress)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.custAddr = custAddr;
+            this.phoneNumber = phoneNumber;
+            this.emailAddress = emailAddress;
+        }
+    }
+}
---
+++ AutoWand/AutoWand/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace AutoWand
+{   //employee class will serve to hold employee information, as well as login information
+    [XmlRoot(ElementName = "Employee")]
+    public class Employee
+    {
+        //employee ID
+        [XmlAttribute(DataType = "string")]
+        public string employeeID { get; set; }
+
+        //employee name info
+        [XmlAttribute(DataType = "string")]
+        public string firstName { get; set; }
+        [XmlAttribute(DataType = "string")]
+        public string lastName { get; set; }
+
+        //employee login information
+        [XmlAttribute(DataType = "string")]
+        public string userName { get; set; }
+        [XmlAttribute(DataType = "string")]
+        public string password { get; set; }
+        
+        //employee permissions   A: Admin,   M: Manager,   E: Employee
+        [XmlAttribute(DataType = "string")]
+        public char employeePerm { get; set; }
+
+        public Employee()
+        {
+
+        }
+
+        public Employee(string employeeID, string firstName, string lastName, string userName, string password, char employeePerm)
+        {
+            this.employeeID = employeeID;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.userName = userName;
+            this.password = password;
+            this.employeePerm = employeePerm;
+        }
+    }
+}
