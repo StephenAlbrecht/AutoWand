74daa401d3da7d94386fac7783f8d4b1f1700e1b
Stephen Albrecht <stephen.b.albrecht@gmail.com>
2018-10-14 18:15:15 -0400

    Implemented layout and partial backend for Login window

--- AutoWand/AutoWand/EmployeeLogin.xaml.cs
+++ AutoWand/AutoWand/EmployeeLogin.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Serialization;
 
 namespace AutoWand
 {
@@ -20,9 +21,37 @@ namespace AutoWand
     /// </summary>
     public partial class EmployeeLogin : Window
     {
+        public List<int> Users = new List<int>(); // change to user class
+        XmlSerializer xmler = new XmlSerializer(typeof(List<int>));
+
         public EmployeeLogin()
         {
             InitializeComponent();
+            ReadInUsers();
+        }
+
+        private void ReadInUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LogInClick(object sender, RoutedEventArgs e)
+        {
+            // if fields valid
+            if (true)
+            {
+                CustomerLookup lookupWin = new CustomerLookup(); // pass users
+            }
+        }
+
+        private void AddNewEmployeeClick(object sender, RoutedEventArgs e)
+        {
+            // if fields valid and user is admin
+            if (true)
+            {
+                AddNewEmployee newEmployeeWin = new AddNewEmployee(); // pass users
+                newEmployeeWin.Show();
+            }
         }
     }
 }
