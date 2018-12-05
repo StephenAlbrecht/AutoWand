a234da77dc2bc78910f0438e494be6304e6e8e44
StephenAlbrecht <stephen.b.albrecht@gmail.com>
2018-12-04 21:56:07 -0500

    Partial implementation of Add Service Window

---
+++ AutoWand/AutoWand/AddEditServiceDisplay.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutoWand
+{
+    /// <summary>
+    /// Interaction logic for AddEditServiceDisplay.xaml
+    /// </summary>
+    public partial class AddEditServiceDisplay : UserControl
+    {
+        public AddEditServiceDisplay()
+        {
+            AddServiceDisplayVM addServiceVM = new AddServiceDisplayVM();
+            InitializeComponent();
+            DataContext = addServiceVM;
+        }
+    }
+}
---
+++ AutoWand/AutoWand/VewModels/AddServiceDisplayVM.cs
@@ -0,0 +1,93 @@
+﻿using PetShop.Classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Xml.Serialization;
+
+namespace AutoWand
+{
+    public class AddServiceDisplayVM : INotifyPropertyChanged
+    {
+        List<Service> Services = new List<Service>();
+        XmlSerializer xmler = new XmlSerializer(typeof(List<Service>));
+
+        public AddServiceDisplayVM()
+        {
+            ReadInServices();
+        }
+
+        // Properties
+        private string _serviceName;
+        public string ServiceName
+        {
+            get { return _serviceName; }
+            set
+            {
+                _serviceName = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("ServiceName"));
+            }
+        }
+
+        private string _partsCost;
+        public string PartsCost
+        {
+            get { return _partsCost; }
+            set
+            {
+                _partsCost = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("PartsCost"));
+            }
+        }
+
+        private string _laborCost;
+        public string LaborCost
+        {
+            get { return _laborCost; }
+            set
+            {
+                _laborCost = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("LaborCost"));
+            }
+        }
+
+        private void ReadInServices()
+        {
+            string path = "Services.xml";
+            if (File.Exists(path))
+            {
+                using (FileStream readStream = new FileStream(path, FileMode.Open, FileAccess.Read))
+                {
+                    Services = xmler.Deserialize(readStream) as List<Service>;
+                }
+            }
+        }
+
+
+        private void AddService(object sender)
+        {
+
+        }
+        
+
+        public ICommand AddServiceCommand
+        {
+            get
+            {
+                if (addServiceEvent == null)
+                {
+                    addServiceEvent = new DelegateCommand(AddService);
+                }
+                return addServiceEvent;
+            }
+        }
+        DelegateCommand addServiceEvent;
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
